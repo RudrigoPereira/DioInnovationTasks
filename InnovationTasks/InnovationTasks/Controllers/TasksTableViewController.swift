4208dc2deda42f01a2f3f47d2ab0613e40e7fb4c
Rodrigo Ferreira Pereira <rudrigopereira@gmail.com>
2023-05-19 00:29:37 -0300

    Iniciando um app agenda

---
+++ InnovationTasks/InnovationTasks/Controllers/TasksTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TasksTableViewController.swift
+//  InnovationTasks
+//
+//  Created by Rodrigo Ferreira Pereira on 19/05/23.
+//
+
+import UIKit
+
+class TasksTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
